46757edbd53b689b3b7e9fcc318de62036e1ee9d
Renato Jema <vadimeche@gmail.com>
2023-09-26 01:55:28 +0300

    f

---
+++ app/app.py
@@ -0,0 +1,42 @@
+import sys
+import PyQt6 as Qt
+import design
+from PyQt6.QtWidgets import QMessageBox
+from PyQt6.QtCore import QCoreApplication
+from math import cos, sin, tan, pi
+
+class App(design.Ui_Form, Qt.QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.buttonResult.clicked.connect(self.resultHandler)
+        self.buttonClear.clicked.connect(self.clearHandler)
+        self.buttonClose.clicked.connect(self.closeHandler)
+
+    def resultHandler(self):
+        if self.boxA.text() == '':
+            QMessageBox.about(self, "Неверные данные", "Неверные данные")
+            return
+        a = float(self.boxA.text().replace(',', '.'))
+        depth = self.boxComma.value()
+        res1 = (sin(4*a))/(1+cos(4*a)) * (cos(2*a))/(1+cos(2*a)) 
+        res2 = 1/tan(3/2*pi - a)
+        QMessageBox.about(self, "Result", ("Z1: " + ("{0:." + str(depth) +"f}").format(res1))+"\n"+("Z2: " +("{0:." + str(depth) +"f}").format(res2)))
+
+
+    def clearHandler(self):
+        self.boxA.clear()
+        self.boxComma.setValue(2)
+
+    def closeHandler(self):
+        QCoreApplication.quit()
+
+    
+def main():
+    app = Qt.QtWidgets.QApplication(sys.argv)
+    window = App()
+    window.show()
+    app.exec()
+
+if __name__ == '__main__':
+    main()
---
+++ app/convert.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+pyuic6 design.ui -o design.py
